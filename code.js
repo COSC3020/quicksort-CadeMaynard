99ca513c57e055b21288e5bee683c62f4e9157f5
CadeMaynard <143521268+CadeMaynard@users.noreply.github.com>
2023-10-14 22:29:48 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,35 @@
 function quicksort(array) {
+    /* New Idea: Everything is a pivot!*/
+    for(i = 0; i < array.length; i++){
+        pivot = i;
+        for(b = 0; b < array.length; b++){
+            if(b != pivot){
+                if(array[b] < array[pivot] && b > pivot){
+                    temp = array[b];
+                    for(c = b; c >= pivot; c--)
+                            array[c] = array[c - 1]
+                    array[pivot] = temp;
+                } else if(array[b] > array[pivot] && b < pivot){
+                    temp = array[pivot];
+                    for(c = pivot; c <= b; c++)
+                            array[c] = array[c + 1]
+                    array[pivot] = temp;
+                } else if(array[b] == array[pivot]){    // From here...
+                    if(b > pivot) {
+                        temp = array[b];
+                        for(c = b; c > pivot; c--)
+                            array[c] = array[c - 1]
+                        array[pivot] = temp;
+                        pivot++;
+                    } else{
+                        temp = array[pivot];
+                        for(c = pivot; c < b; c--)
+                            array[c] = array[c - 1]
+                        array[b] = temp;
+                    }
+                }                                       // ...to here is completely optional, it just makes the function stable.
+            }
+        }
+    }
     return array;
 }
